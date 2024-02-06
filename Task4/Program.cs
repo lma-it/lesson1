003fa829e2519a5e8ad14070a1da727c6ff3beca
Михаил Лубенцов <wispergently061@gmail.com>
2024-02-06 14:55:47 +0500

    add lesson1:task4

---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = 15;
+int b = 33;
+int c = 7;
+int d = 14;
+int e = 24;
+
+int max = a;
+
+if (b > max){
+    max = b;
+}
+if (c > max){
+    max = c;
+}
+if (d > max){
+    max = d;
+}
+if (e > max){
+    max = e;
+}
+Console.WriteLine(max);
